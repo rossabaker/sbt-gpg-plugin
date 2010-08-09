9de8507e4059ecee5b5c46530ba5dfb27ff4b27c
Ross A. Baker <baker@alumni.indiana.edu>
2010-08-08 23:00:13 -0400

    Revert "[issue #2] Odious hack to add signatures to artifacts by var."
    This reverts commit e76900adeeca7dc9dd0cbd3e83ec9dab08b10712.

--- src/main/scala/GpgPlugin.scala
+++ src/main/scala/GpgPlugin.scala
@@ -11,40 +11,29 @@ trait GpgPlugin extends BasicManagedProject {
 
   val signaturesConfig = config("signatures") 
 
-  // Crude hack.  Issue #2: we don't want signatures to be artifacts if 
-  // we're just running 'publish-local', or else the build fails until 
-  // you've published.
-  // 
-  // A var in your build might void the SBT warranty.  Would love a
-  // cleaner solution.
-  var signaturesAreArtifacts = false 
-
-  override def artifacts = 
-    if (signaturesAreArtifacts) { 
-      super.artifacts flatMap { artifact =>      
-        val ascArtifact = Artifact(artifact.name, "asc", artifact.extension+".asc", artifact.classifier, Seq(signaturesConfig), None)
-        Seq(artifact, ascArtifact)
-      }
-    } else {
-      super.artifacts
-    }
+  override def artifacts = super.artifacts flatMap { artifact =>
+    val ascArtifact = Artifact(artifact.name, "asc", artifact.extension+".asc", artifact.classifier, Seq(signaturesConfig), None)
+    Seq(artifact, ascArtifact)
+  }
 
   def signAction = signTask(artifacts) 
     .dependsOn(super.publishAction.dependencies : _*)
     .describedAs("Signs each artifact with GnuPG.")
 
   def signTask(artifacts: Iterable[Artifact]): Task = task {
-    val result = artifacts.toStream flatMap signArtifact firstOption;
-    signaturesAreArtifacts = true
-    result
+    artifacts.toStream flatMap signArtifact firstOption
   }
 
   def signArtifact(artifact: Artifact): Option[String] = {
     val path = artifact2Path(artifact)
-    List(gpgCommand, "-ab", "--yes", path).mkString(" ") ! match {
-      case 0 => None
-      case _ => Some("error signing artifact: "+path)
-    }    
+    path.ext match {
+      case "asc" => None
+      case _ =>
+        List(gpgCommand, "-ab", "--yes", path).mkString(" ") ! match {
+          case 0 => None
+          case _ => Some("error signing artifact: "+path)
+        }    
+    }
   }
 
   protected def artifact2Path(artifact: Artifact): Path = {
