1d2839d46cfceb804b95ec8b9d77882c94ae9a07
Ross A. Baker <baker@alumni.indiana.edu>
2010-08-11 12:53:41 -0400

    Deploy releases to a releases repository.

--- project/build/GpgPluginProject.scala
+++ project/build/GpgPluginProject.scala
@@ -2,6 +2,10 @@ import sbt._
 
 class GpgPluginProject(info: ProjectInfo) extends PluginProject(info) {
   override def managedStyle = ManagedStyle.Maven
-  val publishTo = "Scala-tools Nexus Snapshots" at "http://nexus.scala-tools.org/content/repositories/snapshots"
+  val publishTo = 
+    if (version.toString.endsWith("-SNAPSHOT"))
+      "Scala-tools Snapshots" at "http://nexus.scala-tools.org/content/repositories/snapshots" 
+    else 
+      "Scala-tools Releases" at "http://nexus.scala-tools.org/content/repositories/releases"
   Credentials(Path.userHome / ".ivy2" / "credentials" / "nexus.scala-tools.org", log)
 }
